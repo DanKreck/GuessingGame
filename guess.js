09ea1863d4b9b78cb239dbc7b1bc4901ef8137eb
Daniel Kreck <Kreck.Daniel@gmail.com>
2021-03-11 21:17:49 -0500

    Create guess.js

---
+++ guess.js
@@ -0,0 +1,34 @@
+//Welcome message
+console.log('Hello, Welcome to the guessing game.');
+
+//New line
+console.log("\n");
+
+console.log("I'm thinking of a 7 letter word.");
+//
+let answer = "comcast";
+//Takes user input and assigns it to guess variable
+const input = require('readline-sync');
+let guess = input.question("Please guess a letter: ");
+//The user enters a guess
+console.log(guess);
+
+//If statement that checks answer for the letter that was guessed
+if (answer.includes(guess)) {
+   console.log("Yes. The word contains that letter.");
+} else {
+   console.log("The word does not contain the letter", (guess) );
+}
+//New line
+console.log("\n");
+
+///Would you like to solve?
+console.log("Would you like to solve the word?")
+const solve = require('readline-sync');
+let final = input.question("What is the secret word?:");
+
+if (final.includes (answer)){
+  console.log ("Congrats! that's the word!");
+} else {
+  console.log ("No. That is not the word.")
+}
